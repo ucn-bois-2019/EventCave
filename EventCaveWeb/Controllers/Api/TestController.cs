cd7671f98b8b9a4aa6e960be22ce864e4af7d1ca
Marek Vargovčík <vargovcik.marek@gmail.com>
2019-12-02 14:44:08 +0100

    modified .gitignore and added test controller

---
+++ EventCaveWeb/Controllers/Api/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace EventCaveWeb.Controllers.Api
+{
+    [RoutePrefix("Api/Test")]
+    public class TestController : ApiController
+    {
+        [Route("")]
+        public object Get()
+        {
+            return new { firstName = "LMAO", lastName = "topKek", array = new string[] { "value1", "value2" } };
+        }
+
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+    }
+}
