47dc959fb966c0f98de7b6a4f5b91387d7219812
Marek Vargovčík <vargovcik.marek@gmail.com>
2019-12-02 11:35:36 +0100

    imtegration of web api into project

---
+++ EventCaveWeb/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using EventCaveWeb.Utils;
+using System.Web.Http;
+
+namespace EventCaveWeb
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            var formatter = new BrowserJsonFormatter();
+            formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            config.Formatters.Add(formatter);
+        }
+    }
+}
